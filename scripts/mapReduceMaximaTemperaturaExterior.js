65128bd6efdfac397a071db8a72e6f2a4f219bed
Iñigo Serrano <inigo.serrano@gmail.com>
2014-09-22 18:52:14 +0200

    Frist version

---
+++ scripts/mapReduceMaximaTemperaturaExterior.js
@@ -0,0 +1,9 @@
+db.weather.mapReduce(
+function(){ 
+    emit(this.Time.split(" ")[0],this.Outdoor_Temperature)
+    },
+function(key, values){ 
+    return Math.max.apply(null,values)
+    },
+{ out: "maximaTemperaturaExterior"})
+
---
+++ scripts/mapReduceMediaTemperaturaExterior.js
@@ -0,0 +1,9 @@
+db.weather.mapReduce(
+function(){ 
+    emit(this.Time.split(" ")[0],this.Outdoor_Temperature)
+    },
+function(key, values){ 
+    return Array.sum(values)/values.length
+    },
+{ out: "mediaTemperaturaExterior"})
+
