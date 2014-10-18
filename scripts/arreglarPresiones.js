85cef95b677bbba29e0a9042a135904891487d09
Iñigo Serrano <inigo.serrano@gmail.com>
2014-10-18 19:39:23 +0200

    Arreglar datos

---
+++ scripts/arreglarPresiones.js
@@ -0,0 +1,6 @@
+db.weather.update({ Absolute_Pressure: "---" },
+                  { $set: {Absolute_Pressure: 0} },
+                  {multi: true});
+db.weather.update({ Relative_Pressure: "---" },
+                  { $set: {Relative_Pressure: 0} },
+                  {multi: true});
---
+++ scripts/arreglarTime.js
@@ -0,0 +1,10 @@
+db.weather.find().forEach(function(doc) { 
+    var fecha = doc.Time.split(" ");
+    var dia = fecha[0].split("-")[0];
+    var mes = fecha[0].split("-")[1];
+    var anio = fecha[0].split("-")[2];
+    var hora = fecha[1].split(":")[0];
+    var minuto = fecha[1].split(":")[1];
+    doc.Time = new ISODate(anio+"-"+mes+"-"+dia+"T"+fecha[1]+":00.000Z");
+    db.weather.save(doc); 
+    })
